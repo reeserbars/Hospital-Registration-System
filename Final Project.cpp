cf27d75490aac7c6906e34e64451ba1ee4d11083
reeserbars <snickerweeniespace2006@gmail.com>
2024-12-01 20:34:53 +0800

    added lotsa comments

--- Final Project.cpp
+++ Final Project.cpp
@@ -1,12 +1,12 @@
-#include <iostream>
-#include <fstream>
-#include <vector>
-#include <sstream>
-#include <string>
+#include <iostream>  // For input and output functions (cin, cout)
+#include <fstream>   // For file handling (ifstream, ofstream)
+#include <vector>    // For storing and managing lists of patients
+#include <sstream>   // For parsing strings from file input
+#include <string>    // For handling string data types
 
 using namespace std;
 
-struct Patient {
+struct Patient { //struct is a datatype that groups together related variables
 	int id{};
 	string name;
 	int age{};
@@ -16,15 +16,15 @@ struct Patient {
 };
 
 //function prototypes
-void menu();
-void displayAllPatients();
-void searchPatientById();
-void addPatient();
-void deletePatientById();
-void editPatientById();
-void savePatients(const vector<Patient>& patients);
-vector<Patient> loadPatients();
-int getIntInput(const string& prompt);
+void menu();                       // Displays main menu and handles user choice
+void displayAllPatients();          // Displays all patients from the file
+void searchPatientById();           // Searches for a patient by their ID
+void addPatient();                  // Adds a new patient to the file
+void deletePatientById();           // Deletes a patient by their ID
+void editPatientById();             // Edits patient information based on ID
+void savePatients(const vector<Patient>& patients); // Saves patients to file
+vector<Patient> loadPatients();     // Loads patient data from file
+int getIntInput(const string& prompt); // Gets integer input with validation
 
 int main() {
 	menu();
@@ -56,23 +56,86 @@ void menu() {
 	} while (choice != 6);
 }
 
-// Function to display all patients
+// Loads patient data from file
+vector<Patient> loadPatients() {
+	
+	vector<Patient> patients; // defines array of patient structs named patients
+	ifstream file("patients.csv");
+
+	if (file.is_open()) { // if file open is successful aka if it exists or not
+		string line;	//for holding each line of text
+		while (getline(file, line)) { // loop continues until there are no more lines to read
+			stringstream ss(line); // defines string streamvariable and sets it to the line
+			Patient patient; // patient struct named patient
+			string age, id; 
+
+
+			//reads values from stringstream
+			getline(ss, id, ',');	// its like cin but instead of input it reads from whatever string you set the stringstream to
+			getline(ss, patient.name, ',');
+			getline(ss, age, ',');
+			getline(ss, patient.gender, ',');
+			getline(ss, patient.contact, ',');
+			getline(ss, patient.medicalHistory, ',');
+
+			patient.id = stoi(id); //stoi means string to integer so its static cast but fancy
+			patient.age = stoi(age);
+			patients.push_back(patient); // puts the patient object into the patient info
+		}
+		file.close();
+	} else { // if it cant find the file or something around that
+		cout << "Error opening file for reading." << endl;
+	}
+	return patients; // returns the patient array to whatever called it
+}
+
+// Displays all patients from the csv file
 void displayAllPatients() {
 	vector<Patient> patients = loadPatients();
 	if (patients.empty()) {
 		cout << "No patient records found." << endl;
 	} else {
-		for (const auto& patient : patients) {
-			cout << "| ID: " << patient.id << "\n| Name: " << patient.name << "\n| Age: " << patient.age
-				 << "\n| Gender: " << patient.gender << "\n| Contact: " << patient.contact
+	
+	// this is the equivalent its just fancier because i used a range based for loop
+	// 	for (int i = 0; i < patients.size(); ++i) {
+    // 		const Patient& patient = patients[i];  
+	//
+	// syntax: for (variable-that-holds-every-value-in-the-a-container : container-where-each-element-is-put-into-the-variable-every-iteration)
+	//
+	// auto is there so you dont have to specify the type for every variable since the patients array stores different datatypes 
+	// 
+	//& copies the value of the array to the current var, const so that we dont accidentally modify it, because using & directly affects the variable since it IS the variable,
+	//its essentially another name for the variable. using it saves on memory because it doesnt make a copy of the original variable which would be pretty memory intensive
+		for (const auto& patient : patients) { 
+			cout << "| ID: " << patient.id 
+				<< "\n| Name: " << patient.name 
+				<< "\n| Age: " << patient.age
+				 << "\n| Gender: " << patient.gender 
+				 << "\n| Contact: " << patient.contact
 				 << "\n| Medical History: " << patient.medicalHistory << endl << endl;
 		}
 	}
 }
 
+int getIntInput(const string& prompt) { // & used again here because large strings go brr on memory, const so that no accidental modification
+	int value;
+	//infinite while loop until user gives valid input
+	while (true) {
+		cout << prompt; // outputs prompt strng that was given as arg by caller
+		if (cin >> value) break; // read input and if statement and its code in one line. Yes you can do that. easy input validation didnt even need cin.fail()
+		else {
+			cout << "Please enter a valid number." << endl;
+			cin.clear();
+			cin.ignore(10000, '\n');
+		}
+	}
+	cin.ignore(); // clears any extra input
+	return value;
+}
+
 // Function to search for a patient by ID
 void searchPatientById() {
-	int id = getIntInput("Enter the ID of the patient to search: ");
+	int id = getIntInput("Enter the ID of the patient to search: "); //calls get input
 	vector<Patient> patients = loadPatients();
 	bool found = false;
 
@@ -166,35 +229,6 @@ void savePatients(const vector<Patient>& patients) {
 	}
 }
 
-vector<Patient> loadPatients() {
-	vector<Patient> patients;
-	ifstream file("patients.csv");
-
-	if (file.is_open()) {
-		string line;
-		while (getline(file, line)) {
-			stringstream ss(line);
-			Patient patient;
-			string age, id;
-
-			getline(ss, id, ',');
-			getline(ss, patient.name, ',');
-			getline(ss, age, ',');
-			getline(ss, patient.gender, ',');
-			getline(ss, patient.contact, ',');
-			getline(ss, patient.medicalHistory, ',');
-
-			patient.id = stoi(id);
-			patient.age = stoi(age);
-			patients.push_back(patient);
-		}
-		file.close();
-	} else {
-		cout << "Error opening file for reading." << endl;
-	}
-	return patients;
-}
-
 void addPatient() {
 	ofstream file("patients.csv", ios::app);
 	Patient patient;
@@ -220,17 +254,3 @@ void addPatient() {
 	file.close();
 }
 
-int getIntInput(const string& prompt) {
-	int value;
-	while (true) {
-		cout << prompt;
-		if (cin >> value) break;
-		else {
-			cout << "Please enter a valid number." << endl;
-			cin.clear();
-			cin.ignore(10000, '\n');
-		}
-	}
-	cin.ignore();
-	return value;
-}
